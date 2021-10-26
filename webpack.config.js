23a4cbfa5574c093be13a06db2111911ebbb1177
Somdotta07 <jhilick.s@gmail.com>
2021-10-26 16:55:00 +0200

    Add basic structure
    -Add index.html
    -Add style.css
    -Add index.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,16 @@
+/* eslint-disable linebreak-style */
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
+  mode: 'development',
   entry: './src/index.js',
   devServer: {
     static: './dist',
   },
   plugins: [
     new HtmlWebpackPlugin({
-        template: './src/index.html'
+      template: './src/index.html',
     }),
   ],
   output: {
