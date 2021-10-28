98ab172e2945a3046ed3868215737264ee54aeef
Somdotta07 <jhilick.s@gmail.com>
2021-10-28 18:31:49 +0200

    Add line-through
    add text-decoration
    add condition in  js  file

--- src/interactive.js
+++ src/interactive.js
@@ -2,11 +2,16 @@
 
 export default function interactive(event) {
   const task = JSON.parse(localStorage.getItem('task'));
-  const text = event.currentTarget.nextElementSibling.textContent;
-  const toDos = task.filter((task) => task.description === text)[0];
+  const text = event.currentTarget.nextElementSibling;
+  const toDos = task.filter((task) => task.description === text.textContent)[0];
   const id = task.indexOf(toDos);
 
   task[id].completed = event.target.checked;
+  if (event.target.checked) {
+    text.classList.add('linethrough');
+  } else {
+    text.classList.remove('linethrough');
+  }
 
   localStorage.setItem('task', JSON.stringify(task));
 }
