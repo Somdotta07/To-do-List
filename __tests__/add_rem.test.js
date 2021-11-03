8e92324631a46274a48cbe993171ac16d782c05c
Timowest12 <timowester@gmail.com>
2021-11-03 16:28:58 +0100

    Add addtodo tests localstorage mock

--- __tests__/add_rem.test.js
+++ __tests__/add_rem.test.js
@@ -1,36 +1,35 @@
 /* eslint-disable linebreak-style */
 import addToDo from '../src/add_rem.js';
-
-test('Test the function', () => {
-  // const taskList = [];
-  // Arrange
-  const taskList = [
-    {
-      description: 'Task 1',
-      completed: false,
-      index: 1,
-    },
-    {
-      description: 'Task 2',
-      completed: false,
-      index: 2,
-    },
-  ];
-  // Mock function
-  const addToDoMocked = jest.fn((taskList, taskInput) => {
-    if (taskInput === '') {
-      return;
-    }
-    const taskElement = {
-      description: taskInput,
-      completed: false,
-      index: taskList.length + 1,
-    };
-    taskList.push(taskElement);
-    // return taskList.length;
-    // addTOLocalStorage('task', taskList);
-    // indexValue();
-  });
-  // Act
-  expect(addToDoMocked(taskList, 'New Task').length).toBe(3);
-});
+describe('tests',()=>{
+  test('Test the function', () => {
+    // const taskList = [];
+    // Arrange
+    const taskList = [
+      {
+        description: 'Task 1',
+        completed: false,
+        index: 1,
+      },
+      {
+        description: 'Task 2',
+        completed: false,
+        index: 2,
+      },
+    ];
+    // Mock function
+    const addToDoMocked = jest.fn((taskList, taskInput) => {
+      if (taskInput === '') {
+        return;
+      }
+      const taskElement = {
+        description: taskInput,
+        completed: false,
+        index: taskList.length + 1,
+      };
+      taskList.push(taskElement);
+       return taskList.length;
+    });
+    // Act
+    expect(addToDoMocked(taskList, 'New Task')).toBe(3);
+  })
+})
