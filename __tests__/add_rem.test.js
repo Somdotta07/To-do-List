4ac0282bf4516af4693510239908ef8d2b9466b8
Timo <timowester@gmail.com>
2021-11-03 17:20:57 +0100

    Update add_rem.test.js

--- __tests__/add_rem.test.js
+++ __tests__/add_rem.test.js
@@ -1,36 +1,55 @@
-/* eslint-disable linebreak-style */
-import addToDo from '../src/add_rem.js';
-
-test('Test the function', () => {
-  // const taskList = [];
-  // Arrange
-  const taskList = [
-    {
-      description: 'Task 1',
-      completed: false,
-      index: 1,
-    },
-    {
-      description: 'Task 2',
-      completed: false,
-      index: 2,
-    },
-  ];
-  // Mock function
-  const addToDoMocked = jest.fn((taskList, taskInput) => {
-    if (taskInput === '') {
-      return;
-    }
-    const taskElement = {
-      description: taskInput,
-      completed: false,
-      index: taskList.length + 1,
-    };
-    taskList.push(taskElement);
-    // return taskList.length;
-    // addTOLocalStorage('task', taskList);
-    // indexValue();
+describe('tests', () => {
+  test('add the task', () => {
+    // const taskList = [];
+    // Arrange
+    const taskList = [
+      {
+        description: 'Task 1',
+        completed: false,
+        index: 1,
+      },
+      {
+        description: 'Task 2',
+        completed: false,
+        index: 2,
+      },
+    ];
+    // Mock function
+    const addToDoMocked = jest.fn((taskList, taskInput) => {
+      if (taskInput === '') {
+        return;
+      }
+      const taskElement = {
+        description: taskInput,
+        completed: false,
+        index: taskList.length + 1,
+      };
+      taskList.push(taskElement);
+      // eslint-disable-next-line
+      return taskList.length; 
+    });
+    // Act
+    expect(addToDoMocked(taskList, 'New Task')).toBe(3);
   });
-  // Act
-  expect(addToDoMocked(taskList, 'New Task').length).toBe(3);
+  test('delteitem', () => {
+    const tasks = [
+      {
+        description: 'Task 1',
+        completed: false,
+        index: 1,
+      },
+      {
+        description: 'Task 2',
+        completed: false,
+        index: 2,
+      },
+    ];
+    const deleteitem = jest.fn((index, tasks) => {
+      tasks.splice(index, 1);
+      // eslint-disable-next-line
+      return tasks.length; 
+      //return
+    });
+    expect(deleteitem(0, tasks)).toBe(1);
+  });
 });
