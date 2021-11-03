bfd0047c7960bb4e09ed1996e594659948fb4b85
Somdotta07 <jhilick.s@gmail.com>
2021-11-03 16:01:08 +0100

    make changes

--- __tests__/add_rem.test.js
+++ __tests__/add_rem.test.js
@@ -1,9 +1,36 @@
 /* eslint-disable linebreak-style */
 import addToDo from '../src/add_rem.js';
 
-
-test('Test the functions', () => {
-  const localStorage = '';
-  const value = 'Laundry';
-  expect(addToDo(value)).not.toBe('Done');
+test('Test the function', () => {
+  // const taskList = [];
+  // Arrange
+  const taskList = [
+    {
+      description: 'Task 1',
+      completed: false,
+      index: 1,
+    },
+    {
+      description: 'Task 2',
+      completed: false,
+      index: 2,
+    },
+  ];
+  // Mock function
+  const addToDoMocked = jest.fn((taskList, taskInput) => {
+    if (taskInput === '') {
+      return;
+    }
+    const taskElement = {
+      description: taskInput,
+      completed: false,
+      index: taskList.length + 1,
+    };
+    taskList.push(taskElement);
+    // return taskList.length;
+    // addTOLocalStorage('task', taskList);
+    // indexValue();
+  });
+  // Act
+  expect(addToDoMocked(taskList, 'New Task').length).toBe(3);
 });
