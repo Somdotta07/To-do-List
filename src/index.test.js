bda0e76c5d5ce5f24ed06287a19f4d1b3370c3a9
Timowest12 <timowester@gmail.com>
2021-11-04 16:17:11 +0100

    Mock html output

--- src/index.test.js
+++ src/index.test.js
@@ -1,135 +1,26 @@
-decribe('DOM manipulation', () => {
+describe('DOM manipulation', () => {
   test('Test for output HTML', () => {
-    let task = [];
-
-    const getList = () => {
-      if (getFromLocalStorage('task') == null) {
-        addTOLocalStorage('task', task);
-      }
-      task = getFromLocalStorage('task');
+      
+      let task = [{
+        description: 'Task1',
+        completed: false,
+        index: 1,
+      }];
+      let shouldreturn = '<ul><li><input type="checkbox"><p>Task1</p></li></ul>';
+    let htmlmock = '<ul></ul>';
+      const getList = (htmlmock,task) => {
+          const document = htmlmock;
+          let storage = [];
       task.sort((a, b) => a.index - b.index);
 
-      const itemContainer = document.querySelector('ul');
-      itemContainer.innerHTML = '';
-
       for (let i = 0; i < task.length; i += 1) {
-        // List
-        const list = document.createElement('li');
-        itemContainer.appendChild(list);
-
-        // Inside List create a div
-        const listContainer = document.createElement('div');
-        listContainer.classList.add('list-con');
-        list.appendChild(listContainer);
-
-        // Input checkBox
-        const checkBox = document.createElement('input');
-        checkBox.setAttribute('type', 'checkbox');
-        listContainer.appendChild(checkBox);
-        // Text item
-        const description = document.createElement('p');
-        description.textContent = task[i].description;
-        listContainer.appendChild(description);
-        // input for paragraph
-        const editPara = document.createElement('input');
-        editPara.value = task[i].description;
-        editPara.classList.add('none');
-        editPara.setAttribute('type', 'text');
-        listContainer.appendChild(editPara);
-        if (task[i].completed) {
-          description.classList.add('linethrough');
-        }
-        // Icon
-        const verIcon = document.createElement('i');
-        verIcon.classList.add('fas');
-        verIcon.classList.add('fa-ellipsis-v');
-        // verIcon.addEventListener('click', (e) => {
-        //   removeLine(e);
-        // });
-        list.appendChild(verIcon);
-
-        const trash = document.createElement('i');
-        trash.classList.add('fas');
-        trash.classList.add('fa-trash-alt');
-        trash.classList.add('none');
-        // trash.addEventListener('click', () => {
-        //   deleteItem(i);
-        //   getList();
-        // });
-        list.appendChild(trash);
+          let adderstring = '<li>'+'<input type="checkbox"><p>'+task[i].description+'</p>'+'</li>';
+          storage.push(adderstring);
       }
+      storage = '<ul>'+storage+'</ul>';
+      return storage;
     };
+    expect(getList(htmlmock,task)).toBe(shouldreturn)
   });
+
 });
-let task = [];
-
-const getList = () => {
-  if (getFromLocalStorage('task') == null) {
-    addTOLocalStorage('task', task);
-  }
-  task = getFromLocalStorage('task');
-  task.sort((a, b) => a.index - b.index);
-
-  const itemContainer = document.querySelector('ul');
-  itemContainer.innerHTML = '';
-
-  for (let i = 0; i < task.length; i += 1) {
-    // List
-    const list = document.createElement('li');
-    itemContainer.appendChild(list);
-
-    // Inside List create a div
-    const listContainer = document.createElement('div');
-    listContainer.classList.add('list-con');
-    list.appendChild(listContainer);
-
-    // Input checkBox
-    const checkBox = document.createElement('input');
-    checkBox.setAttribute('type', 'checkbox');
-    // checkBox.checked = task[i].completed;
-    // checkBox.addEventListener('change', (event) => {
-    //   const text = event.currentTarget.nextElementSibling;
-    //   interactive(i, event.target.checked);
-    //   if (event.target.checked) {
-    //     text.classList.add('linethrough');
-    //   } else {
-    //     text.classList.remove('linethrough');
-    //   }
-    // });
-    listContainer.appendChild(checkBox);
-    // Text item
-    const description = document.createElement('p');
-    description.textContent = task[i].description;
-    listContainer.appendChild(description);
-    // input for paragraph
-    const editPara = document.createElement('input');
-    editPara.value = task[i].description;
-    editPara.classList.add('none');
-    editPara.setAttribute('type', 'text');
-    // editPara.addEventListener('blur', (e) => {
-    //   editText(e.currentTarget.value, i);
-    // });
-    listContainer.appendChild(editPara);
-    if (task[i].completed) {
-      description.classList.add('linethrough');
-    }
-    // Icon
-    const verIcon = document.createElement('i');
-    verIcon.classList.add('fas');
-    verIcon.classList.add('fa-ellipsis-v');
-    verIcon.addEventListener('click', (e) => {
-      removeLine(e);
-    });
-    list.appendChild(verIcon);
-
-    const trash = document.createElement('i');
-    trash.classList.add('fas');
-    trash.classList.add('fa-trash-alt');
-    trash.classList.add('none');
-    trash.addEventListener('click', () => {
-      deleteItem(i);
-      getList();
-    });
-    list.appendChild(trash);
-  }
-};
